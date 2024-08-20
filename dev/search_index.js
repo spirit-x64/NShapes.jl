94748feda976c710390723c57f44ebe745dd5162
Documenter.jl <documenter@juliadocs.github.io>
2024-08-20 08:42:16 +0000

    build based on 3f93e4c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = NShapes","category":"page"},{"location":"#NShapes","page":"Home","title":"NShapes","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for NShapes.jl.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [NShapes]","category":"page"}]
+}
